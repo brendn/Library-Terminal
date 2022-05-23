b74b9966ad3d3364562874e8ad5ebd374fdf0b0a
brendn <brendn@users.noreply.github.com>
2022-05-23 19:45:16 -0400

    Eclipse is evil

--- src/co/grandcircus/library/Library.java
+++ src/co/grandcircus/library/Library.java
@@ -1,5 +1,10 @@
 package co.grandcircus.library;
 
 public class Library {
+	
+	public static void main(String[] args) {
+		Inventory inventory = new Inventory();
+		System.out.println(inventory.getMainBookList());
+	}
 
 }
