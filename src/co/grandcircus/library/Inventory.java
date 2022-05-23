bbbf1d0039559067f2b202556a5d35d258845acf
nickringe <nickringe@gmail.com>
2022-05-23 19:38:50 -0400

    Added Inventory Method

--- src/co/grandcircus/library/Inventory.java
+++ src/co/grandcircus/library/Inventory.java
@@ -1,5 +1,18 @@
 package co.grandcircus.library;
 
+import java.util.ArrayList;
+
 public class Inventory {
 
+	private ArrayList<Book> mainBookList;
+
+	public ArrayList<Book> getMainBookList() {
+		return mainBookList;
+	}
+	
+	public void addBook(Book book) {
+		this.mainBookList.add(book);
+	}
+	
+	
 }
