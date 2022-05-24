4adfa6e81869db5a1529356a0e4e075662fc9ee0
nickringe <nickringe@gmail.com>
2022-05-23 20:55:40 -0400

    Added initial TDD

--- src/co/grandcircus/library/Book.java
+++ src/co/grandcircus/library/Book.java
@@ -1,73 +1,76 @@
 package co.grandcircus.library;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Book {
-	
+
 	private final String title;
 	private final String author;
 	private Status status;
-	private int condition = 10;
+	private int condition;
 	private Date dueDate = null;
-	
+
 	public Book(String title, String author, Status status, int condition) {
 		this.title = title;
 		this.author = author;
 		this.status = status;
 		this.condition = condition;
 	}
-	
+
 	public Book(String title, String author, Status status) {
 		this(title, author, status, 10);
 	}
-	
+
 	public enum Status {
 		ON_SHELF, CHECKED_OUT;
 	}
-	
+
 	public void checkIn() {
 		if (condition <= 0) {
 			condition = 10;
-			System.out.println("RECYCLED");
+			System.out.println(this.title + " has been RECYCLED. Condition reset to 10.");
 		}
-	}
-	
-	public void checkOut() {
-		condition--;
+		this.status = Status.ON_SHELF;
 	}
 
+	public void checkOut() {
+		condition--;
+		this.status = Status.CHECKED_OUT;
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DATE, 14);
+		setDueDate(calendar.getTime());
+		System.out.println(this.title + " is due on: " + calendar.getTime());
+
+
+	}
 
 	public String getTitle() {
 		return title;
 	}
 
-
 	public String getAuthor() {
 		return author;
 	}
-
 
 	public Status getStatus() {
 		return status;
 	}
 
-
 	public void setStatus(Status status) {
 		this.status = status;
 	}
 
-
 	public int getCondition() {
 		return condition;
 	}
-	
+
 	public Date getDueDate() {
 		return dueDate;
 	}
-	
+
 	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
-	
 
 }
--- src/co/grandcircus/library/Tests.java
+++ src/co/grandcircus/library/Tests.java
@@ -1,5 +1,92 @@
 package co.grandcircus.library;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import co.grandcircus.library.Book.Status;
+
 public class Tests {
 
+	@Test
+	void conditionStartsAt10Test() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.CHECKED_OUT);
+		int expected = 10;
+		int actual = book.getCondition();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void conditionIsDepreciatingTest() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.CHECKED_OUT);
+		book.checkIn();
+		book.checkOut();
+		int expected = 9;
+		int actual = book.getCondition();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void conditionIsDepreciatingTest2() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.ON_SHELF);
+		book.checkOut();
+		book.checkIn();
+		int expected = 9;
+		int actual = book.getCondition();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void conditionIsDepreciatingTest3() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.ON_SHELF, 2);
+		book.checkOut();
+		book.checkIn();
+		int expected = 1;
+		int actual = book.getCondition();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void conditionIsRecyclingTest() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.ON_SHELF, 1);
+		book.checkOut();
+		book.checkIn();
+		int expected = 10;
+		int actual = book.getCondition();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void statusIsCorrectTest() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.ON_SHELF, 1);
+		book.checkOut();
+		Status expected = Status.CHECKED_OUT;
+		Status actual = book.getStatus();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void statusIsCorrectTest2() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.CHECKED_OUT, 1);
+		book.checkIn();
+		Status expected = Status.ON_SHELF;
+		Status actual = book.getStatus();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void dueDateTest() {
+		Book book = new Book("Hitchiker's Guide To The Galaxy", "Douglas Adams", Status.ON_SHELF, 1);
+		book.checkOut();
+		Calendar calendar = Calendar.getInstance();
+		Date expected = book.getDueDate();
+		Date actual = book.getDueDate();
+		assertEquals(expected, actual);
+	}
+	
+	
+	
 }
