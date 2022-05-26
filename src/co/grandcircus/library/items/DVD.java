2b111cc1d96e2b740414af570e53cfae45e4935e
nickringe <nickringe@gmail.com>
2022-05-26 19:08:43 -0400

    Building the Preview database

--- src/co/grandcircus/library/items/DVD.java
+++ src/co/grandcircus/library/items/DVD.java
@@ -67,39 +67,4 @@ public class DVD extends Media {
 			System.out.println("Error at DVD class/preview() method. Please try again or call Customer Support if error persists.");
 		}
 	}
-	
-	
-	//CREATE THE MEDIA ARRAYLIST
-	//ArrayList<Media> mediaList = new ArrayList<>();
-	//mediaList.add(goonies);
-	//mediaList.add(lostBoys);
-	//mediaList.add(standByMe);
-	//mediaList.add(attackOnTitan);
-	//mediaList.add(all of the books as well)...
-	
-	//CREATE THE PREVIEW ARRAYLISTS FOR EACH DVD
-//	ArrayList<String> gooniesPreview = new ArrayList<>();
-//	gooniesPreview.add("Front Cover: A bunch of scared-looking kids crowded around a Pirate skull and gold coins!");
-//	gooniesPreview.add("Inside Cover: 'Don't say that, ever say that. Goonies never say die.'");
-//	gooniesPreview.add("Back Cover: A deliciously dizzy adventure with crowd-pleasing fun!");
-//	ArrayList<String> lostBoysPreview = new ArrayList<>();
-//	lostBoysPreview.add("Front Cover: A group of tough-looking young-adults staring you menacingly in the eyes.");
-//	lostBoysPreview.add("Inside Cover: 'You'll never grow old Michael, and you'll never die. But you must feed!'");
-//	lostBoysPreview.add("Back Cover: A hip, comic twist on classic vampire stories");
-	//ArrayList<String> standByMePreview = new ArrayList<>();
-	//standByMePreview.add("Front Cover: Four young boys painted above a backdrop of open wilderness");
-	//standByMePreview.add("Inside Cover: 'I never had any friends later on like the ones I had when I was twelve...'");
-	//standByMePreview.add("Back Cover: Absolutely wonderful...a film I may never forget!");
-	//ArrayList<String> attackOnTitanPreview = new ArrayList<>();
-	//standByMePreview.add("Front Cover: Two people stand at the foot of a massive crystal-laden cavern; a third person is chained to the walls above them.");
-	//attackOnTitanPreview.add("Inside Cover: 'This world is merciless, and it's also very beautiful.'");
-	//attackOnTitanPreview.add("Back Cover: Lies uphold these walls.");
-	
-	//CREATE THE DVD OBJECTS
-//	DVD goonies = new DVD("The Goonies", ON_SHELF, gooniesPreview, "Richard Donner");
-//	DVD lostBoys = new DVD("The Lost Boys", ON_SHELF, lostBoysPreview, "Joel Schumacher" );
-	//DVD standByMe = new DVD("Stand By Me", ON_SHELF, standByMePreview, "Rob Reiner");
-	//DVD attackOnTitan = new DVD("Attack On Titan: Season 3", ON_SHELF, attackOnTitanPreview, "Masashi Koizuka");
-
-
 }
